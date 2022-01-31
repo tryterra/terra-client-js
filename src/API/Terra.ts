292d4156a860ce9ef16a0c1d9bed56ab8a4e8757
JaafarRammal <jarammal@gmail.com>
2022-01-31 17:05:22 +0000

    add docs

--- src/API/Terra.ts
+++ src/API/Terra.ts
@@ -25,14 +25,37 @@ export default class Terra {
     }
   }
 
+  /**
+   * Sets the current Terra user. Function calls on the instance will be called with this user ID
+   *
+   * @param {string} userID - Terra user ID
+   *
+   */
   setCurrentUser(userID: string): void {
     this.userID = userID;
   }
 
+  /**
+   * Sets the current Terra user set.
+   *
+   * @returns {string} Terra user ID
+   *
+   */
   getCurrentUser(): string {
     return this.userID;
   }
 
+  /**
+   * Generate a widget session
+   *
+   * @param {string} referenceID - Terra user ID
+   * @param {string[]} providers - Array of strings for wearable providers
+   * @param {string} language - Language the widget page is showed in
+   * @param {string} auth_success_redirect_url - Redirect URL when the session succeeds
+   * @param {string} auth_failure_redirect_url - Redirect URL when the session fails
+   *
+   * @returns {Promise<TerraWidgetResponse>} A promise of type Widget Response
+   */
   generateWidgetSession(
     referenceID: string,
     providers: string[],
@@ -51,14 +74,34 @@ export default class Terra {
     );
   }
 
+  /**
+   * Get available Terra providers
+   *
+   * @return {Promise<TerraProvidersResponse>} A promise of type Providers
+   *
+   */
   getProviders(): Promise<TerraProvidersResponse> {
     return GetProviders();
   }
 
+  /**
+   * Get all subscribed users
+   *
+   * @return {Promise<TerraSubscriptionsResponse>} A promise of type Subscriptions
+   *
+   */
   getUsers(): Promise<TerraSubscriptionsResponse> {
     return GetSubscribers(this.devID, this.apiKey);
   }
 
+  /**
+   * Get information about a Terra user
+   *
+   * @param {string} userID - Terra user ID
+   *
+   * @return {Promise<TerraUserResponse>} A promise of type User
+   *
+   */
   getUser(userID: string): Promise<TerraUserResponse> {
     return GetUser(this.devID, this.apiKey, userID);
   }
@@ -84,13 +127,64 @@ export default class Terra {
     };
   }
 
+  /**
+   * Get Athlete data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   *
+   * @return {Promise<TerraAthleteResponse>} A promise of type Athlete Data
+   *
+   */
   getAthlete(toWebhook?: boolean): Promise<TerraAthleteResponse> {
     return GetAthlete(this.devID, this.apiKey, this.userID, toWebhook);
   }
 
+  /**
+   * Get Activity data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   * @param {Date} startDate - Start date for the date range limit
+   * @param {Date} endDate - End dat for the date range limit
+   * @return {Promise<TerraDataResponse<Activity>>} A promise of type Activity Data
+   *
+   */
   getActivity = this.getDataWrapper<Activity>("activity");
+
+  /**
+   * Get Body data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   * @param {Date} startDate - Start date for the date range limit
+   * @param {Date} endDate - End dat for the date range limit
+   * @return {Promise<TerraDataResponse<Body>>} A promise of type Body Data
+   *
+   */
   getBody = this.getDataWrapper<Body>("body");
+
+  /**
+   * Get Daily data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   * @param {Date} startDate - Start date for the date range limit
+   * @param {Date} endDate - End dat for the date range limit
+   * @return {Promise<TerraDataResponse<Daily>>} A promise of type Daily Data
+   *
+   */
   getDaily = this.getDataWrapper<Daily>("daily");
+
+  /**
+   * Get Sleep data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   * @param {Date} startDate - Start date for the date range limit
+   * @param {Date} endDate - End dat for the date range limit
+   * @return {Promise<TerraDataResponse<Sleep>>} A promise of type Sleep Data
+   *
+   */
   getSleep = this.getDataWrapper<Sleep>("sleep");
+
+  /**
+   * Get Menstruation data for current user
+   * @param {boolean} toWebhook - True for sending data to webhook and false for sending data in response body
+   * @param {Date} startDate - Start date for the date range limit
+   * @param {Date} endDate - End dat for the date range limit
+   * @return {Promise<TerraDataResponse<Menstruation>>} A promise of type Menstruation Data
+   *
+   */
   getMenstruation = this.getDataWrapper<Menstruation>("menstruation");
 }
