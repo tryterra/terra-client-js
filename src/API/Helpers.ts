94ebd98e94bd33b6a73115bfe5f6e809b1e626ca
JaafarRammal <jarammal@gmail.com>
2022-10-13 21:16:32 +0100

    fix date string

--- src/API/Helpers.ts
+++ src/API/Helpers.ts
@@ -2,12 +2,19 @@ import { URL } from "url";
 import { TerraPayload } from "./WebhookEvents";
 import fetch from "cross-fetch";
 
+function ZeroPad(value: number, minSize: number) {
+  return (
+    "0".repeat(Math.max(minSize - value.toString().length, 0)) +
+    value.toString()
+  );
+}
+
 export function DateToTerraDate(d: Date): string {
   var month = d.getUTCMonth() + 1; //months from 1-12
   var day = d.getUTCDate();
   var year = d.getUTCFullYear();
 
-  return year + "-" + month + "-" + day;
+  return year + "-" + ZeroPad(month, 2) + "-" + ZeroPad(day, 2);
 }
 
 export function RequestWrapper<T>(
