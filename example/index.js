c011a350192331aeb6d3e5b363452ce5e0188bef
Jaafar Rammal <jarammal@gmail.com>
2022-12-21 11:22:08 +0000

    docs (example): add comments to the example

--- example/index.js
+++ example/index.js
@@ -1,17 +1,20 @@
+// import terra
 const { default: Terra } = require('terra-api');
+
+// load a config
 require('dotenv').config();
 
-// setup a new object instance
+// setup a new object instance using the config
 const terra = new Terra(process.env.DEV_ID, process.env.API_KEY, process.env.SECRET);
 
-// ---------------------------
-// Server
-// ---------------------------
+// setup a server using express
 const express = require('express');
-const bodyParser = require('body-parser');
-const { randomInt } = require('crypto');
 const app = express();
 const PORT = 3000;
+
+// this is important, as it allows you to use rawBody for the signature verification
+// bodyParser will change the req.body from req.rawBody
+const bodyParser = require('body-parser');
 app.use(
   bodyParser.json({
     verify: (req, _, buf) => {
@@ -19,84 +22,48 @@ app.use(
     },
   }),
 );
+
+// start the server
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
+// a timeout helper function
 function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// a function that handles the webhooks
+// the function simulates e.g. a transaction to a data store, which takes a random time
+// the time is set between 0 and 2 seconds
+// randomly, the function will also throw an error
+// this is to simulate the handler throwing errors as well to ensure later on they are catched
+const { randomInt } = require('crypto');
 async function handleWebhook(req) {
   await timeout(randomInt(3) * 1000);
   if (randomInt(3) === 1) throw 'some error';
   console.log(req.url);
 }
 
-// Webhook port
+// a constant signaling if we should handle webhooks one by one or not
+HANDLE_SYNC = true
+
+// expose POST /hook endpoint on the server
+// this is where Terra will send webhooks
 app.post('/hook', (req, res) => {
-  console.log('registering', req.url);
-  // handleWebhook(req) // async, random order processing that might result in race conditions
-  terra.executeSynchronously(handleWebhook, req); // sync, process one by one
+  // verify the signature of the payload
   if (!terra.checkTerraSignature(req.headers['terra-signature'], req.rawBody)) res.sendStatus(401);
   res.sendStatus(200);
+
+  // handle the payload
+  // handle synchonously the payloads one by one as they come in, avoiding race conditions
+  if(HANDLE_SYNC) terra.executeSynchronously(handleWebhook, req);
+  // race conditions cannot occure, allow async handling
+  else handleWebhook(req);
 });
 
-app.get('/', (_, res) => {
-  res.send('Hello Terra!');
-});
-
+// expose GET /session endpoint to generate a widget session
 app.get('/session', (req, res) => {
   terra
     .generateWidgetSession('refID', 'EN', ['GARMIN', 'FITBIT'])
     .then((s) => res.send(s.url))
     .catch((e) => console.log(e));
 });
-
-terra
-  .generateWidgetSession('refID', 'EN')
-  .then((s) => console.log(s.url))
-  .catch((e) => console.log(e));
-
-terra
-  .generateAuthToken()
-  .then((s) => console.log(s))
-  .catch((e) => console.log(e));
-
-terra
-  .authUser('INBODY', 'reference', undefined, undefined, undefined, '1234')
-  .then((r) => console.log(r))
-  .catch((e) => console.log(e));
-
-terra
-  .getUsers()
-  .then((res) => {
-    console.log(res);
-    res.users.forEach((u) => {
-      if (u.provider == 'OURA') {
-        terra
-          .getAthlete(u.user_id, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-        terra
-          .getBody(u.user_id, new Date(), undefined, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-        terra
-          .getDaily(u.user_id, new Date(), undefined, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-        terra
-          .getMenstruation(u.user_id, new Date(), undefined, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-        terra
-          .getSleep(u.user_id, new Date(), undefined, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-        terra
-          .getActivity(u.user_id, new Date(), undefined, false)
-          .then((r) => console.log(r))
-          .catch((e) => console.log(e));
-      }
-    });
-  })
-  .catch((e) => console.log(e));
