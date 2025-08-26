6c89e3982c2424899999ce9a098994362b3bdb31
Sim Shang En <6071014+12458@users.noreply.github.com>
2025-08-26 09:01:08 -0400

    fix: update index.ts to export verifyTerraWebhookSignature

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * as Terra from "./api/index.js";
 export { TerraError, TerraTimeoutError } from "./errors/index.js";
 export { TerraClient } from "./Client.js";
 export { TerraEnvironment } from "./environments.js";
+export { verifyTerraWebhookSignature } from "./utils.js";
